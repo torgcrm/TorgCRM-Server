0a97b63b5551edf1e746c55bedf21c00f9462b60
Ilya Durdyev <funbanji@gmail.com>
2017-03-11 15:27:05 +0300

    Generate JHipster Project #TCRM_CE-1 Done

---
+++ src/main/webapp/app/shared/shared.module.ts
@@ -0,0 +1,53 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import { CookieService } from 'angular2-cookie/services/cookies.service';
+import {
+    TorgCrmceSharedLibsModule,
+    TorgCrmceSharedCommonModule,
+    CSRFService,
+    AuthService,
+    AuthServerProvider,
+    AccountService,
+    UserService,
+    StateStorageService,
+    LoginService,
+    LoginModalService,
+    Principal,
+    HasAnyAuthorityDirective,
+    JhiLoginModalComponent
+} from './';
+
+@NgModule({
+    imports: [
+        TorgCrmceSharedLibsModule,
+        TorgCrmceSharedCommonModule
+    ],
+    declarations: [
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective
+    ],
+    providers: [
+        CookieService,
+        LoginService,
+        LoginModalService,
+        AccountService,
+        StateStorageService,
+        Principal,
+        CSRFService,
+        AuthServerProvider,
+        AuthService,
+        UserService,
+        DatePipe
+    ],
+    entryComponents: [JhiLoginModalComponent],
+    exports: [
+        TorgCrmceSharedCommonModule,
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective,
+        DatePipe
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
+})
+export class TorgCrmceSharedModule {}
